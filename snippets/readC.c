a6673312bdba5e5c23644dfd02f8829b604f425a
Ben Page <benjaminpage.acer@gmail.com>
2024-01-04 15:49:20 +0000

    Updated readC.c to correct version from learnLyncs.

--- snippets/readC.c
+++ snippets/readC.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <complex.h>
 #include <string.h>
-
+#include <errno.h>
+#include <time.h>
 
 #define ND 4
 #define NC 3
@@ -13,16 +15,41 @@ void ConjTranspose(double complex M[NC][NC]);
 int idx(int pos[], int shape[], int Nd);
 void construct_3x3(double complex M[3][3], double complex U[], int idx);
 
-int main(double complex U[], int Nt, int Ns){
+int main(){
     // Calculates the average value of the trace of the spatial 
     // and temporal plaquettes.
     // Assumes Nd=4 and Nc=3
     
-    int nP = 0; // The number of plaquettes
+    int Nt = 8;
+    int Ns = 24;
+    
+    // Open pointer to binary file containing data
+    FILE *fptr = fopen("Gen2_8x24_gfAr0.C","r");
+    //FILE *fptr = fopen("testfile", "r");
+
+    if( fptr == NULL ) {
+        fprintf(stderr, "Couldn't open %s: %s\n", "Gen2_8x24_gfAr0.C", strerror(errno));
+        exit(1);
+    }
+ 
+    int nmemb = Nt*Ns*Ns*Ns*ND*NC*NC; // Lattice volume
+    int size = sizeof(double complex);
+
+    // Allocate memory to store the lattice
+    double complex *U = (double complex*) malloc(nmemb * size);
+    
+    // Read the gaugefields into U
+    fread(U, size, nmemb, fptr);
+    fclose(fptr);
+    
+    int S_nP = 0; // The number of spatial plaquettes
+    int T_nP = 0; // The number of temporal plaquettes
    
     double S_sumReTrP = 0; // Sum of real trace of spatial plaquettes
     double T_sumReTrP = 0; // Sum of real trace of temporal plaquettes
-    
+ 
+    clock_t start = clock();
+
     // Loop over all sites
     for(int t=0; t<Nt; t++){
         for(int i=0; i<Ns; i++){
@@ -36,19 +63,30 @@ int main(double complex U[], int Nt, int Ns){
                     
                     for(int nu=1; nu<ND; nu++){
                         T_sumReTrP += plaquette(U, pos, mu, nu, Nt, Ns);
+                        T_nP += 1;
                     }
                     
                     // Loop over spatial Lorentz indices
                     for(int mu=1; mu<ND; mu++){
                         for(int nu=mu+1; nu<ND; nu++){
                             S_sumReTrP += plaquette(U, pos, mu, nu, Nt, Ns);
+                            S_nP += 1;
                         }
                     }
                 }
             }
         }
     }
-    
+
+    clock_t end = clock();
+
+    printf("Type:\t\tsumReTrP:\tnP:\tAvg:\n");
+    printf("whole\t\t%lf\t%d\t%lf\n", S_sumReTrP+T_sumReTrP,S_nP+T_nP,(S_sumReTrP+T_sumReTrP)/(S_nP+T_nP));
+    printf("spatial\t\t%lf\t%d\t%lf\n", S_sumReTrP,S_nP,S_sumReTrP/S_nP);
+    printf("temporal\t%lf\t%d\t%lf\n", T_sumReTrP,T_nP,T_sumReTrP/T_nP);
+    printf("Total execution time: %fs\n",(float)(end-start)/CLOCKS_PER_SEC);
+
+    return EXIT_SUCCESS;
     
 };
 
@@ -58,12 +96,12 @@ double plaquette(double complex U[], int pos[], int mu, int nu, int Nt, int Ns){
     
     int U_shape[DIM] = {Nt, Ns, Ns, Ns, ND, NC, NC}; // Shape of the lattice
     
-    double plaq[3][3]; // Plaquette matrix
+    double complex plaq[3][3]; // Plaquette matrix
     
-    double U_mu[3][3];    // Working matrices for calculations
-    double U_nu[3][3];
-    double U_mu_dag[3][3];
-    double U_nu_dag[3][3];
+    double complex U_mu[3][3];    // Working matrices for calculations
+    double complex U_nu[3][3];
+    double complex U_mu_dag[3][3];
+    double complex U_nu_dag[3][3];
     
     // Get position of U_mu on the lattice
     int U_mu_pos[DIM] = {pos[0], pos[1], pos[2], pos[3], mu, 0, 0}; // Start SU(3) indices at zero
@@ -74,7 +112,7 @@ double plaquette(double complex U[], int pos[], int mu, int nu, int Nt, int Ns){
     
     // Get neighbour in the mu-direction
     int next_pos[ND];
-    memcpy(next_pos, pos, sizeof(pos));
+    memcpy(next_pos, pos, sizeof(int)*4); // Explicitly copy pos
     next_pos[mu] += 1;
     if(mu == 0){ next_pos[mu] %= Nt; }else{ next_pos[mu] %= Ns; } // Enforce periodicity
     
@@ -89,7 +127,7 @@ double plaquette(double complex U[], int pos[], int mu, int nu, int Nt, int Ns){
     MultiplyMat(plaq, U_mu, U_nu);
     
     // Get neighbour in the nu-direction
-    memcpy(next_pos, pos, sizeof(pos));
+    memcpy(next_pos, pos, sizeof(int)*4);
     next_pos[nu] += 1;
     if(nu == 0){ next_pos[nu] %= Nt; }else{ next_pos[nu] %= Ns; } // Enforce periodicity
     
@@ -106,7 +144,7 @@ double plaquette(double complex U[], int pos[], int mu, int nu, int Nt, int Ns){
     int U_nu_dag_idx = idx(U_nu_dag_pos, U_shape, DIM); // Calculate address in memory of U_nu^\dag
 
     // Populate U_nu_dag with value of U_nu and transpose
-    construct_3x3(U_nu_dag, U, U_mu_dag_idx);
+    construct_3x3(U_nu_dag, U, U_nu_dag_idx);
     ConjTranspose(U_nu_dag);
     
     // Temporarily store U_mu^\dag(x+nu) * U_nu^\dag in U_mu
@@ -215,4 +253,3 @@ void construct_3x3(double complex M[3][3], double complex U[], int idx){
         }
     }
 };
-
