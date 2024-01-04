3e301c1657a13b4d438b63d6ef0434e9f17a8c42
RJaBi <ryan07bignell+github@gmail.com>
2024-01-04 15:51:20 +0000

    now works

--- src/wilsonLoops.c
+++ src/wilsonLoops.c
@@ -4,10 +4,11 @@
 #include <time.h>
 
 
-void one_x_one(double complex U[], int pos[], int mu, int nu, int Nt, int Ns, double complex plaq[NC][NC]){
+
+void one_x_one(double complex U[], int pos[], int mu, int nu, int Nt, int Ns, double complex plaq[3][3]){
     // Calculate the value of the plaquette
     // in the mu-nu plane at position pos.
-
+    
     int U_shape[DIM] = {Nt, Ns, Ns, Ns, ND, NC, NC}; // Shape of the lattice
     
     //double complex plaq[3][3]; // Plaquette matrix
@@ -19,22 +20,25 @@ void one_x_one(double complex U[], int pos[], int mu, int nu, int Nt, int Ns, do
     
     // Get position of U_mu on the lattice
     int U_mu_pos[DIM] = {pos[0], pos[1], pos[2], pos[3], mu, 0, 0}; // Start SU(3) indices at zero
-    int U_mu_idx = idx(U_mu_pos, U_shape, DIM); // Calculate address in memory of U_mu    
+    int U_mu_idx = idx(U_mu_pos, U_shape, DIM); // Calculate address in memory of U_mu
+    
     // Populate U_mu with value of U_mu
     construct_3x3(U_mu, U, U_mu_idx);
-    // up to here is fine
+    
     // Get neighbour in the mu-direction
     int next_pos[ND];
-    memcpy(next_pos, pos, sizeof(int)*4);  // Explicitly copy pos
+    memcpy(next_pos, pos, sizeof(int)*4); // Explicitly copy pos
     next_pos[mu] += 1;
     if(mu == 0){ next_pos[mu] %= Nt; }else{ next_pos[mu] %= Ns; } // Enforce periodicity
     
     // Get position of U_nu(x+mu) on the lattice
     int U_nu_pos[DIM] = {next_pos[0], next_pos[1], next_pos[2], next_pos[3], nu, 0, 0};
     int U_nu_idx = idx(U_nu_pos, U_shape, DIM); // Calculate address in memory of U_nu
+    
     // Populate U_nu with value of U_nu(x+mu)
     construct_3x3(U_nu, U, U_nu_idx);
-    // Calculate U_mu (plaq) * U_nu(x+mu) (plaq)
+    
+    // Calculate U_mu (plaq) * U_nu(x+mu) (work)
     MultiplyMat(plaq, U_mu, U_nu);
     
     // Get neighbour in the nu-direction
@@ -55,7 +59,7 @@ void one_x_one(double complex U[], int pos[], int mu, int nu, int Nt, int Ns, do
     int U_nu_dag_idx = idx(U_nu_dag_pos, U_shape, DIM); // Calculate address in memory of U_nu^\dag
 
     // Populate U_nu_dag with value of U_nu and transpose
-    construct_3x3(U_nu_dag, U, U_mu_dag_idx);
+    construct_3x3(U_nu_dag, U, U_nu_dag_idx);
     ConjTranspose(U_nu_dag);
     
     // Temporarily store U_mu^\dag(x+nu) * U_nu^\dag in U_mu
@@ -64,27 +68,8 @@ void one_x_one(double complex U[], int pos[], int mu, int nu, int Nt, int Ns, do
     
     // Calculate full plaquette;
     MultiplyMat(plaq, plaq, U_mu);
-
-      double trace = 0;
-    
-      for(int i=0; i<NC; i++){
-	trace += U_mu[i][i];
-      }
-      //printf("%f\n", creal(trace));
-
-    
-    // Return the real part of the trace
-    /*
-        double trace = 0;
-    
-    for(int i=0; i<NC; i++){
-        trace += plaq[i][i];
-    }
-    
-    return creal(trace);
-    */
-    // return plaq;
 }
+
 
 int main(){
   // does stuff
