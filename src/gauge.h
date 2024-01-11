9befe6d5b0b0fbba64a6657723c2e88fd55a2eaa
asmecca <antoniosmecca@gmail.com>
2024-01-11 19:05:37 +0100

    restored to c

--- src/gauge.h
+++ src/gauge.h
@@ -1,22 +1,19 @@
-// #include <stdlib.h>  // or this ?
-#include <cmath>    // Not sure what this is for
-#include <complex>  // For complex numbers
-#include <cstdio>
-// #include <stdio.h>   // For FILE
-#include <cerrno>   // the errno in opening gaugefield
-#include <cstring>  // for strerror
-#include <iostream>
-typedef std::complex<double> dc;
+#include <complex.h>  // For complex numbers
+#include <errno.h>    // the errno in opening gaugefield
+#include <math.h>     // Not sure what this is for
+#include <stdio.h>    // For FILE
+#include <stdlib.h>   // or this ?
+#include <string.h>   // for strerror
 
 #define DIM 7  // Rank of the gauge field array Nt x Ns^3 x Nd x Nc^2
 #define NC 3   // NC = 3 only
 #define ND 4   // ND = 4 only
 
-void MultiplyMat(dc MM[NC][NC], dc left[NC][NC],
-                 dc right[NC][NC]);  // 3x3 only
+void MultiplyMat(double complex MM[NC][NC], double complex left[NC][NC],
+                 double complex right[NC][NC]);  // 3x3 only
 
-void readGauge_C(int NS, int NT, const char* filename, dc* U);
+void readGauge_C(int NS, int NT, const char* filename, double complex* U);
 int idx(int pos[], int shape[], int Nd);
 
-void construct_3x3(dc M[3][3], dc U[], int idx);
-void ConjTranspose(dc M[NC][NC]);
+void construct_3x3(double complex M[3][3], double complex U[], int idx);
+void ConjTranspose(double complex M[NC][NC]);
