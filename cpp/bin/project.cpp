909d06a1977ee0fe8604cf483f59c2b86591b702
RJaBi <ryan07bignell+github@gmail.com>
2024-01-12 15:34:03 +0000

    First commit for parallelism using std C++. It doesnt work (runs but gives incorrect answer), but it does iterate the correct number of times, mostly likely over the correct values

--- cpp/bin/project.cpp
+++ cpp/bin/project.cpp
@@ -4,10 +4,56 @@
 #include <ctime>
 // #include <time.h>
 
+#include<ranges>
+#include<execution>
+#include<algorithm>
+#include<numeric>
+
+//#include<ranges.hpp>
+//#include <cartesian_product.hpp>
+
 #include "../src/gauge.h"
 #include "../src/wilsonLoops.h"
 
 using namespace std;
+
+double pathSum(int NS, int NT, int RTPathSize, int* fPathRT, int* bPathRT, dc U[]){
+  // Does the loop over all paths
+  // We construct an enumerate of the cartesian product
+  // The enumerate is required for the transform_reduce
+  auto myRange = std::views::enumerate(std::views::cartesian_product(
+								     std::ranges::views::iota(0,NT),
+								     std::ranges::views::iota(0,NS),
+								     std::ranges::views::iota(0,NS),
+								     std::ranges::views::iota(0,NS)
+								     ));
+
+
+  // mySum holds all of the sum_{paths} at each  (t, x, y, z)
+  std::vector<double> mySum(NT * NS * NS * NS);
+  return std::transform_reduce(std::execution::par_unseq, myRange.begin(), myRange.end(), 0, std::plus{}, [NS, NT, RTPathSize, fPathRT, bPathRT, U, mySum = mySum.data()] (auto index){
+      // a lot here
+      // execution method for parallel or not, etc (seq, unseq, par, par_unseq)
+      // start iter
+      // end iter
+      // start value for sum
+      // what operation doing (sum)
+      // capturing most variables by value (which is often a pointer anyway)
+      // caputuring mySum by reference
+      // index is a tuple of <enum, <t, i, j, k>>
+      // Unwrap it in two stages
+      auto [ii, tijk] = index;
+      auto [t, i, j, k] = tijk;
+      // assemble position
+      int pos[4] = {t, i, j, k};
+      //printf("%d
+      // do work
+      mySum[ii] += tracePathWilsonLoop(U, pos, NT, NS, 3, RTPathSize,
+				      fPathRT, bPathRT);
+      return mySum[ii];
+   });
+}
+
 
 int main() {
   // does stuff
@@ -21,15 +67,16 @@ int main() {
   // printf("%s\n", "Successfully allocated U memory");
   //  readGauge_C(NS, NT, "conf/Gen2_8x24_gfAr0.C", U);
   // readGauge_C(NS, NT, "../conf/Gen2P_k278480_128x48_Tune_053n2.C", U);
-  readGauge_C(NS, NT, "../conf/Gen2l_64x32n100.C", U);
+  readGauge_C(NS, NT, "/cosma5/data/do009/dc-bign2/conf/Gen2l_64x32n325.C", U);
 
-  dc plaq[3][3];  // Plaquette matrix
   // Allocate the path arrays with 6 paths of length 2
   int* fPath = allocatePaths(6, 2);
   int* bPath = allocatePaths(6, 2);
   // Set up the paths
   one_x_one_Paths(fPath, bPath);
 
+
+  dc plaq[3][3];  // Plaquette matrix
   int RTPathSize = calculatePathSize(1, 1);
   printf("%s %d \n", "The path length in 1x1 is ", RTPathSize);
   int* fPathRT = allocatePaths(3, RTPathSize);
@@ -49,6 +96,11 @@ int main() {
 
   clock_t start = clock();
 
+
+
+  double mySum = pathSum(NS, NT, RTPathSize, fPathRT, bPathRT, U);
+  printf("mySum is %lf \n", mySum);
+
   // Loop over all sites
   for (int t = 0; t < NT; t++) {
     for (int i = 0; i < NS; i++) {
@@ -62,10 +114,10 @@ int main() {
 
           // Alternative plaq calc
           sumReTrP +=
-              tracePathWilsonLoop(U, pos, NT, NS, 6, 2, fPath, bPath, plaq);
-          printf("%s", "\n");
+              tracePathWilsonLoop(U, pos, NT, NS, 6, 2, fPath, bPath);
+          //printf("%s", "\n");
           sumReTrWRT += tracePathWilsonLoop(U, pos, NT, NS, 3, RTPathSize,
-                                            fPathRT, bPathRT, plaq);
+                                            fPathRT, bPathRT);
 
           // Loop over temporal Lorentz indices
           int mu = 0;
